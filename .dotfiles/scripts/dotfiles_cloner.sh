43abe52cd20b1dee67f2d7b7297e07f647f20193
rivit98 <albert98g@gmail.com>
2024-09-15 20:56:53 +0200

    improvements

--- .dotfiles/scripts/dotfiles_cloner.sh
+++ .dotfiles/scripts/dotfiles_cloner.sh
@@ -2,4 +2,10 @@
 
 git config --global --add safe.directory '*'
 git clone --bare https://github.com/rivit98/dotfiles.git $HOME/.dotfiles
-# chmod +x $HOME/.dotfiles/scripts/*.sh
+
+function config {
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+config checkout master  -- scripts
+chmod +x $HOME/.dotfiles/scripts/*.sh
