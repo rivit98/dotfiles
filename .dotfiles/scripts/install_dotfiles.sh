f683daf77359ef61ec6182a64776a9e955c4f4bf
rivit98 <albert98g@gmail.com>
2024-09-15 23:35:01 +0200

    fix

--- .dotfiles/scripts/install_dotfiles.sh
+++ .dotfiles/scripts/install_dotfiles.sh
@@ -8,13 +8,7 @@ echo "Installing dotfiles"
 function config {
     /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
 
-if config checkout master; then
-    cho "Checked out config."
-else
-    echo "Backing up pre-existing dot files."
-    config checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} .config-backup/{}
-fi
-config checkout master
+mkdir -p .config-backup
+config checkout -f master
 config config status.showUntrackedFiles no
