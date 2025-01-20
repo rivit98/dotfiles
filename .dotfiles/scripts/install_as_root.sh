2fd8bdf55e8597a86d25ced6fac253822af76877
rivit98 <albert98g@gmail.com>
2025-01-20 20:03:03 +0100

    update

--- .dotfiles/scripts/install_as_root.sh
+++ .dotfiles/scripts/install_as_root.sh
@@ -5,11 +5,9 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y --no-install-recommends $(awk '{print $1}' packages.txt)
 
-
 # install chrome https://www.google.pl/intl/pl/chrome/
 # this sets up apt repository for google chrome
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" && dpkg -i google-chrome-stable_current_amd64.deb && rm -f google-chrome-stable_current_amd64.deb
-
 
 # snaps
 snap refresh
@@ -25,8 +23,6 @@ snap install go --classic
 snap install pycharm-community --classic
 snap install intellij-idea-community --classic
 snap install rustrover --classic
-snap install zig --beta --classic
-
+snap install zig --classic
 
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
-
